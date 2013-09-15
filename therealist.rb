1fdf749882fdc65286e74a2d86945ed14e1e2797
Luís Lago <luisalveslago@gmail.com>
2013-09-15 23:16:26 +0100

    can download complete issue that i choose

--- therealist.rb
+++ therealist.rb
@@ -6,14 +6,15 @@ require 'open-uri'
 DATA_DIR = "the_realist" 
 Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
 BASE_realist_URL = "http://www.ep.tc/realist/"
-issue = 01
-page = BASE_realist_URL + "0" + issue.to_s + "/"
+puts "issue"
+issue = gets.chomp
+page = BASE_realist_URL + issue.to_s + "/"
 ISSUE_DIR = DATA_DIR + "/" + issue.to_s
 Dir.mkdir(ISSUE_DIR) unless File.exists?(ISSUE_DIR)
 # this is just a place holder until I figure out how to make it automatic
 number = 01
 pagina = page + "0" + number.to_s + ".jpg"
-		while issue < 10
+		while number < 10
 		open(pagina) {|f|
    File.open("#{ISSUE_DIR}/#{number}.jpg","wb") do |file|
      file.puts f.read
@@ -22,8 +23,19 @@ end }
 
    number = number + 1
    pagina = page + "0" + number.to_s + ".jpg"
-
-   issue = issue + 1 # this still doesn't work
-   page = BASE_realist_URL + "0" + issue.to_s + "/"
 	end
+	
+	number = 10
+pagina = page + number.to_s + ".jpg"
+	while number < 32
+		open(pagina) {|f|
+   File.open("#{ISSUE_DIR}/#{number}.jpg","wb") do |file|
+     file.puts f.read
+end }
+
+
+   number = number + 1
+   pagina = page + number.to_s + ".jpg"
+	end
+
 puts "done"
