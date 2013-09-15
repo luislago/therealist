20cb028d778b339782f60736575af996837d8f7e
Luís Lago <luisalveslago@gmail.com>
2013-09-15 10:03:43 +0100

    under construction and still doesn't work

--- therealist.rb
+++ therealist.rb
@@ -8,15 +8,21 @@ Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
 BASE_realist_URL = "http://www.ep.tc/realist/"
 puts "issue"
 issue = gets.chomp
-page = BASE_realist_URL + issue
+page = BASE_realist_URL + issue + "/"
 ISSUE_DIR = DATA_DIR + "/" + issue
 Dir.mkdir(ISSUE_DIR) unless File.exists?(ISSUE_DIR)
-puts "page number" # this is just a place holder until I figure out how to make it automatic
-number = gets.chomp
+# this is just a place holder until I figure out how to make it automatic
+number = "01"
 pagina = page + "/" + number
+
+while number.to_i < 36
+	
 
 open("#{pagina}.jpg") {|f|
    File.open("#{ISSUE_DIR}/#{number}.jpg","wb") do |file|
      file.puts f.read
    end
 }
+number = number.to_i + 1
+end
+
