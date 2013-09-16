b8ea71b25a805e188fd2a8b2db263905ccd6f3d4
Luís Lago <luisalveslago@gmail.com>
2013-09-16 08:00:20 +0100

    some code cleaning

--- therealist.rb
+++ therealist.rb
@@ -8,34 +8,38 @@ Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
 BASE_realist_URL = "http://www.ep.tc/realist/"
 puts "issue"
 issue = gets.chomp
-page = BASE_realist_URL + issue.to_s + "/"
-ISSUE_DIR = DATA_DIR + "/" + issue.to_s
+page = BASE_realist_URL + issue + "/"
+ISSUE_DIR = DATA_DIR + "/" + issue
 Dir.mkdir(ISSUE_DIR) unless File.exists?(ISSUE_DIR)
+puts "Creating #{ISSUE_DIR} folder"
 # this is just a place holder until I figure out how to make it automatic
 number = 01
 pagina = page + "0" + number.to_s + ".jpg"
-		while number < 10
+		for number in 1...10
+			puts "Downloading page #{number}"
 		open(pagina) {|f|
    File.open("#{ISSUE_DIR}/#{number}.jpg","wb") do |file|
      file.puts f.read
 end }
 
 
-   number = number + 1
+ 
    pagina = page + "0" + number.to_s + ".jpg"
 	end
 	
 	number = 10
 pagina = page + number.to_s + ".jpg"
-	while number < 32
+	for number in 10...33
+		puts "Downloading page #{number}"
 		open(pagina) {|f|
    File.open("#{ISSUE_DIR}/#{number}.jpg","wb") do |file|
      file.puts f.read
 end }
 
 
-   number = number + 1
+  
    pagina = page + number.to_s + ".jpg"
+      
 	end
 
 puts "done"
