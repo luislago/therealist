b901c6e6a895c0a8b27fce5ff195c2a07e377660
luislago <luisalveslago@gmail.com>
2013-12-18 14:26:39 +0000

    Update therealistcomplete.rb

--- therealistcomplete.rb
+++ therealistcomplete.rb
@@ -1,45 +1,15 @@
 require 'rubygems'
-require 'open-uri'
-
-#downloads a "The Realist" issue you choose to your disk
-
-DATA_DIR = "the_realist" 
+require 'nokogiri' #gem to parse html
+require "open-uri"
+DATA_DIR = "The Realist"
 Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
-BASE_realist_URL = "http://www.ep.tc/realist/" #this is the site where the issues are
-puts "issue"
-
-
-
-
-for issue in 1..9 do
-page = BASE_realist_URL + "0" + issue.to_s + "/" #"page" is the webpage where the specified issue is. (ex: issue one is in 
-#http://www.ep.tc/realist/01)
-ISSUE_DIR = DATA_DIR + "/" + issue.to_s
-Dir.mkdir(ISSUE_DIR) unless File.exists?(ISSUE_DIR) #creates folder to store the issue
-puts "Creating #{ISSUE_DIR} folder"
-
-		for number in 1..9 do # this loop downloads the first 9 issues of the magazine
-			pagina = page + "0" + number.to_s + ".jpg" # it defines a specific page of the magazine, starting with 1, it directs to 
-#the url where the .jpg file where the page scan can be found. the format of the first 9 issues in the site, as well as 
-#the format of the first 9 pages of each issue is "00", so, when I'm converting it to string, I need to add a zero
-			puts "Downloading page #{number}"
-		open(pagina) {|f|
-   File.open("#{ISSUE_DIR}/#{number}.jpg","wb") do |file|
-     file.puts f.read
- end}
- 
+BASE_URL = "http://www.ep.tc/realist/"
+page = Nokogiri::HTML(open(BASE_URL))
+rows = page.css('div form div select') #option')#[3]["value"]
+rows.each do |row|
+	hrefs = row.css("option").map{ |a|
+		a["value"]}
+		hrefs[2..161].each do |href|
+puts href 
 end
-for number in 10..total_pages.to_i #to_i turns the number I set before from a string to an integer.
-pagina = page + number.to_s + ".jpg" #we don't need to add the "0" anymore, since all page number have two digits now.
-		puts "Downloading page #{number}"
-		open(pagina) {|f|
-   File.open("#{ISSUE_DIR}/#{number}.jpg","wb") do |file|
-     file.puts f.read
-end }
-
-
-  
-      
-	end
 end
-puts "done"
